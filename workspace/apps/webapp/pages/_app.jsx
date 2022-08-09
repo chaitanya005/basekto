99ec3e9ea838fa8f985b35f685b5d18a63d217a4
chaitanya005 <muvvalachaitanya05@gmail.com>
2022-08-09 19:06:13 +0530

    Add: Google Analytics

--- workspace/apps/webapp/pages/_app.jsx
+++ workspace/apps/webapp/pages/_app.jsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from 'react-query';
 import { ThemeProvider } from '@basketo/web-ui';
 import { Provider } from 'react-redux';
 import { store } from '../app/store';
+import Script from 'next/script';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -27,6 +28,17 @@ function App({ Component, pageProps }) {
     <ThemeProvider>
       <Provider store={store}>
         <QueryClientProvider client={queryClient}>
+          <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+          <Script strategy="lazyOnload">
+              {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+                page_path: window.location.pathname,
+                });
+              `}
+            </Script>
           <Component {...pageProps} />
         </QueryClientProvider>
       </Provider>
