137758cbd4ecae0cf2b8ac4e93f6080ef8ae827b
Jaivrat Das <jaivratdas123456@gmail.com>
2022-12-26 19:22:47 +0530

    Added route & controller to update user Changes to get user by id controller (get by userAddress now)

--- workspace/apps/server/src/routes/user.js
+++ workspace/apps/server/src/routes/user.js
@@ -1,9 +1,10 @@
 var express = require('express');
-const { createUser, getUser, isExist } = require('../controllers/user');
+const { createUser, getUser, updateUser, isExist } = require('../controllers/user');
 var router = express.Router();
 
 router.post('/user/new', createUser);
 router.get('/user/exist', isExist);
 router.get('/user/:id', getUser);
+router.put('/user/:id', updateUser);
 
 module.exports = router;
--- workspace/apps/server/src/utils/db.js
+++ workspace/apps/server/src/utils/db.js
@@ -2,7 +2,7 @@ const Basket = require('../models/basket');
 const User = require('../models/user');
 
 const singleBasket = (_id) => Basket.findOne({ _id });
-const user = (userAddress) => User.findOne({ userAddress });
+const findUserById = (userAddress) => User.findOne({ userAddress });
 const userBaskets = (userAddress) => Basket.find({accountId:userAddress})
 
-module.exports = { singleBasket, user ,userBaskets};
+module.exports = { singleBasket, findUserById, userBaskets};
