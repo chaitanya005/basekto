7878c8ac2fc5c9d1e76dfd04f633d126ded57835
Jaivrat Das <jaivratdas123456@gmail.com>
2023-02-06 13:49:59 +0530

    Update: Close side-drawer when clicked on any item

--- workspace/apps/webapp/Components/Navbar/index.js
+++ workspace/apps/webapp/Components/Navbar/index.js
@@ -197,6 +197,7 @@ const Navbar = () => {
             onClose={() => setIsDrawerOpen(false)}
           >
             <Box
+              onClick={() => setIsDrawerOpen(false)}
               sx={{
                 fontSize: { xs: '14px', md: '20px' },
                 display: 'flex',
@@ -228,6 +229,7 @@ const Navbar = () => {
                       variant="text"
                       color="primary"
                       startIcon={page.icon}
+                      onClick={() => setIsDrawerOpen(false)}
                     >
                       {page.title}
                     </Button>
